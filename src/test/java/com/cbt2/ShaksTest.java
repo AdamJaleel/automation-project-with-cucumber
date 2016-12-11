79dbf99ab94706c718ab2316319b77ea180c7004
Adam Jaleel <shakriyorm@gmail.com>
2016-12-11 14:27:23 -0500

    created a new package and a new class with added message!

---
+++ src/test/java/com/cbt2/ShaksTest.java
@@ -0,0 +1,7 @@
+package com.cbt2;
+
+public class ShaksTest {
+public static void main(String[] args) {
+	System.out.println("This is the real message from the newly created class under newly created packacge");
+}
+}
